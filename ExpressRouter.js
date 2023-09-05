c027c46354a384d2c03b35cc6aa8012dbd053b72
Piyush <sainipiyush8860@gmail.com>
2023-09-05 12:33:39 +0530

    cors changes

--- ExpressRouter.js
+++ ExpressRouter.js
@@ -20,11 +20,20 @@ mongoose.connect(process.env.MODEL_URI).then(()=>{
 app.use(express.json())
 app.use(cookie())
 app.use(express.urlencoded({extended:true}))
-app.use(cors({
-    origin:[process.env.FRONTEND_URL],
-    methods:["GET","POST","PUT","DELETE"],
-    credentials:true 
-}))
+
+// app.use(cors({
+//     origin:[process.env.FRONTEND_URL],
+//     methods:["GET","POST","PUT","DELETE"],
+//     credentials:true 
+// }))
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); // Replace with your frontend URL
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
 
 //USING ROUTES
 app.use(userRouter);
