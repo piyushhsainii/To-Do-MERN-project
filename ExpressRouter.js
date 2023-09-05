c967e8b40f43cac2d5b2aa711780fe3c86a82806
Piyush <sainipiyush8860@gmail.com>
2023-09-05 18:41:02 +0530

    Urlchanges

--- ExpressRouter.js
+++ ExpressRouter.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({extended:true}))
 // }))
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); // Replace with your frontend URL
+    res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL); // Replace with your frontend URL
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
