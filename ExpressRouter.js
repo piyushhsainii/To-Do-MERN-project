6193757a3bb271f75df709bcbd1fbb754b05a6fc
Piyush <sainipiyush8860@gmail.com>
2023-09-05 22:11:28 +0530

    comment credentials

--- ExpressRouter.js
+++ ExpressRouter.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL); // Replace with your frontend URL
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    // res.setHeader('Access-Control-Allow-Credentials', 'true');
     next();
 });
 
