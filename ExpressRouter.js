b8c616a5d4fbc1e868a49923d34a51a1ae7c6a92
Piyush <sainipiyush8860@gmail.com>
2023-09-05 19:29:07 +0530

    cors changes 2

--- ExpressRouter.js
+++ ExpressRouter.js
@@ -21,19 +21,19 @@ app.use(express.json())
 app.use(cookie())
 app.use(express.urlencoded({extended:true}))
 
-// app.use(cors({
-//     origin:[process.env.FRONTEND_URL],
-//     methods:["GET","POST","PUT","DELETE"],
-//     credentials:true 
-// }))
+app.use(cors({
+    origin:[process.env.FRONTEND_URL],
+    methods:["GET","POST","PUT","DELETE"],
+    credentials:true 
+}))
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL); // Replace with your frontend URL
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL); // Replace with your frontend URL
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     res.setHeader('Access-Control-Allow-Credentials', 'true');
+//     next();
+// });
 
 //USING ROUTES
 app.use(userRouter);
