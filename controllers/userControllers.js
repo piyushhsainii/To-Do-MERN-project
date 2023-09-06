b9d8d6f67f2965dfeabe84bc9fda1e6292287940
Piyush <sainipiyush8860@gmail.com>
2023-09-06 20:48:36 +0530

    checking register

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -29,30 +29,31 @@ const login = async (req,res)=>{
     })
 }
 const register = async(req,res)=>{
-    const {name,email,password} = req.body
-    let user = await User.findOne({email})
+    res.send(console.log('request aarhi hai'))
+    // const {name,email,password} = req.body
+    // let user = await User.findOne({email})
 
-    if(user){
-        return res.status(404).send(
-            'User already exists'
-        )
-        }
-        const HashedPassword = await bcrypt.hash(password, 10);
-         user = await User.create({
-            name,email, password:HashedPassword
-        })
+    // if(user){
+    //     return res.status(404).send(
+    //         'User already exists'
+    //     )
+    //     }
+    //     const HashedPassword = await bcrypt.hash(password, 10);
+    //      user = await User.create({
+    //         name,email, password:HashedPassword
+    //     })
 
-        const Token =  jwt.sign({_id:user._id},process.env.SECRET_KEY)
+    //     const Token =  jwt.sign({_id:user._id},process.env.SECRET_KEY)
 
-        res.status(201).cookie('Token',Token,{
-            expires: new Date(Date.now()*10000),
-            maxAge:35000000,
-            sameSite:process.env.NODE_ENV === "DEVELOPMENT"? 'lax' :'none',
-            secure:process.env.NODE_ENV === "DEVELOPMENT"? false : true
-        }).json({
-            sucess:true,
-            message:'Registered sucessfull'
-        })
+    //     res.status(201).cookie('Token',Token,{
+    //         expires: new Date(Date.now()*10000),
+    //         maxAge:35000000,
+    //         sameSite:process.env.NODE_ENV === "DEVELOPMENT"? 'lax' :'none',
+    //         secure:process.env.NODE_ENV === "DEVELOPMENT"? false : true
+    //     }).json({
+    //         sucess:true,
+    //         message:'Registered sucessfull'
+    //     })
     
 }
 
