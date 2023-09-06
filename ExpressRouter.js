5821ea4bc41502179c3ec0c34d1edda973cb326c
Piyush <sainipiyush8860@gmail.com>
2023-09-06 23:37:58 +0530

    testing cors()-2

--- ExpressRouter.js
+++ ExpressRouter.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MODEL_URI).then(()=>{
 app.use(cors({
     origin:"http://localhost:5173",
     methods:["GET","POST","PUT","DELETE"],
-    // credentials:true 
+    credentials:true 
 }))
 
 
